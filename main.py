57db117b8edb761ac7cfe7c0858ee27f94ef179a
Antonio <physrumoa@gmail.com>
2022-03-02 21:13:21 +0000

    very slow loop to get likelihoods

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # P(x) which is our knowledge about the parameters
 # P(data|H(x)) which is computing the born rule of a certain experiment given the parameter
 # update it
-
+import math
 
 import numpy as np
 from scipy.linalg import expm
@@ -63,7 +63,7 @@ def sample_results(n, omega):
     return results
 
 
-no_parameters = 5
+no_parameters = 100
 prior = np.ones(no_parameters)
 weights = np.ones(no_parameters)
 # evol_vector = np.vectorize(evol_vector)
@@ -73,7 +73,8 @@ v = np.array([0, 1]).reshape(-1, 1)
 measure_qubits = np.array([1, 0]).reshape(-1, 1)
 t = np.linspace(0, 10, 100)
 t_0 = 0.0
-t_f = 4.0
+t_f = 5.0
+n_samples = 1000
 
 evolution = np.zeros((len(alpha), len(t), 2, 1), dtype='complex_')
 # the first component is each alpha of the hamiltonian
@@ -91,6 +92,31 @@ prob_0 = np.power(np.abs(np.squeeze(
 #     plt.plot(t, np.real(prob_0[i, :]), label='l')
 #     plt.legend(str(alpha[i]))
 # plt.show()
-sample = sample_results(50000, 0.8)
+samples = sample_results(50000, 0.8)
 
+alpha = [0, 1]
+np.random.seed(1)
+
+no_parameters = 2
+no_samples = 100
+alpha = np.linspace(0, 1, no_parameters)
+samples = sample_results(no_samples, 0.8)
+probs = []
+
+for i, alpha_i in enumerate(alpha):
+    prob_parameters = []
+    for i_sample, sample in enumerate(samples):
+        t = 1/np.abs((np.random.choice(alpha)-np.random.choice(alpha)))
+        while math.isinf(t):
+            t = 1/np.abs((np.random.choice(alpha)-np.random.choice(alpha)))
+        evo = evol_state(H_matrix(alpha_i), v, t)
+        prob_0 = np.power(np.abs(np.conjugate(measure_qubits.T)@evo), 2).ravel()
+        prob_1 = 1 - prob_0
+        if sample == 0.0:
+            prob_parameters.append(prob_0)
+        else:
+            prob_parameters.append(prob_1)
+    probs.append(prob_parameters)
+    
+# probs = np.array(probs).transpose().reshape(no_parameters, no_samples) 
 print("hello")
